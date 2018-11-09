340a21fa423b0cd110a1b32b71b1b522cb44f330
Rashid Khan <github.fliplap@spamgourmet.com>
2018-11-09 15:25:02 -0700

    Add try function

---
+++ canvas_plugin/functions/common/try.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+canvas.register(() => ({
+  name: 'try',
+  aliases: [],
+  help: 'Handle errors gracefully',
+  args: {
+    expression: {
+      resolve: false,
+      aliases: ['fn', '_'],
+      help: 'A Canvas expression to try. If this errors, catch will be returned',
+    },
+    catch: {
+      resolve: false,
+      aliases: ['fn'],
+      help: 'Value to return if expression errors',
+    },
+  },
+  fn: (context, args) => {
+    return args.expression(context).catch(() => args.catch(context));
+  },
+}));
