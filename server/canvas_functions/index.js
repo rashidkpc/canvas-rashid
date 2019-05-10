0c880cee166a44a8a3533aad27e45905c9995772
Rashid Khan <github.fliplap@spamgourmet.com>
2019-05-10 11:14:38 -0700

    Fix some slackimg stuff

--- server/canvas_functions/index.js
+++ server/canvas_functions/index.js
@@ -1,5 +1,6 @@
 import { asteroids } from './asteroids';
 import { sleep } from './sleep';
 import { slackimg } from './slackimg';
+import { eselements } from './eselements';
 
-export const canvasFunctions = [asteroids, sleep, slackimg];
+export const canvasFunctions = [asteroids, sleep, slackimg, eselements];
