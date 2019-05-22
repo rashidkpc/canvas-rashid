e235c4dabeab5e011c44451a2a6ab0c3d8ef5ab8
Rashid Khan <github.fliplap@spamgourmet.com>
2019-05-22 12:05:47 -0700

    Remove unused scripts and eselements function

--- server/canvas_functions/index.js
+++ server/canvas_functions/index.js
@@ -1,6 +1,5 @@
 import { asteroids } from './asteroids';
 import { sleep } from './sleep';
 import { slackimg } from './slackimg';
-import { eselements } from './eselements';
 
-export const canvasFunctions = [asteroids, sleep, slackimg, eselements];
+export const canvasFunctions = [asteroids, sleep, slackimg];
